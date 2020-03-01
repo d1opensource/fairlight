1aea842a6a9a72790118250056791db4c4fd473f
Thomas Dashney <thomas.dashney@d1g1t.com>
2020-02-29 20:49:12 -0500

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: ['./setup-jest.js'],
-  setupFilesAfterEnv: ['./setup-jest-after-env.js'],
+  setupFilesAfterEnv: ['./setup-jest-after-env.tsx'],
   collectCoverageFrom: ['./src/**/*.tsx']
 }
