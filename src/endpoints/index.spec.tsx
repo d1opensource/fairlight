f6c663e252e21cd86c5b10251099906c6a59594c
Aleksandar Kojic <a.kojic12@gmail.com>
2021-01-19 21:24:24 -0500

    add head request to test

--- src/endpoints/index.spec.tsx
+++ src/endpoints/index.spec.tsx
@@ -9,6 +9,10 @@ describe('HttpEndpoints', () => {
   class Endpoints extends HttpEndpoints {
     static basePath = '/base'
 
+    static HEAD(init: Omit<EndpointCreateRequestInit, 'body'>) {
+      return super._head('/endpoint', init)
+    }
+
     static GET(init: Omit<EndpointCreateRequestInit, 'body'>) {
       return super._get('/endpoint', init)
     }
@@ -31,7 +35,7 @@ describe('HttpEndpoints', () => {
   }
 
   test('endpoints by method', () => {
-    for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
+    for (const method of ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
       expect(Endpoints[method]()).toEqual({
         method,
         url: '/base/endpoint'
@@ -54,7 +58,7 @@ describe('HttpEndpoints', () => {
   test('request headers', () => {
     const headers = {test: 'body'}
 
-    for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
+    for (const method of ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
       expect(Endpoints[method]({headers})).toEqual({
         method,
         url: '/base/endpoint',
@@ -73,7 +77,7 @@ describe('HttpEndpoints', () => {
       zero: 0
     }
 
-    for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
+    for (const method of ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
       expect(Endpoints[method]({query})).toEqual({
         method,
         url: '/base/endpoint?empty=&null=null&num=3&str=string&zero=0'
@@ -85,7 +89,7 @@ describe('HttpEndpoints', () => {
   })
 
   test('extra key', () => {
-    for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
+    for (const method of ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
       expect(Endpoints[method]({extraKey: 'test'})).toEqual({
         method,
         url: '/base/endpoint',
@@ -98,7 +102,7 @@ describe('HttpEndpoints', () => {
     test('applying trailing slash to each url', () => {
       ;(Endpoints as any).trailingSlash = true
 
-      for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
+      for (const method of ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
         expect(Endpoints[method]()).toEqual({
           method,
           url: '/base/endpoint/'
@@ -111,7 +115,7 @@ describe('HttpEndpoints', () => {
     test('applying trailing slash to each url', () => {
       ;(Endpoints as any).trailingSlash = true
 
-      for (const method of ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
+      for (const method of ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE']) {
         expect(Endpoints[method]()).toEqual({
           method,
           url: '/base/endpoint/'
