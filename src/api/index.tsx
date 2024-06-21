68f5a439c80f98cfb86b32d96d559a8c321b99d7
vladislave <vla4erprod@gmail.com>
2024-06-21 16:55:54 +0200

    FRON-9073 Update dependencies

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,3 +1,4 @@
+import Observable from 'zen-observable'
 import PushStream from 'zen-push'
 
 import {DEFAULT_FETCH_POLICY, READ_CACHE_POLICIES} from './constants'
@@ -125,7 +126,7 @@ export class Api {
    */
   readCachedResponse = <TResponseBody extends ResponseBody>(
     params: ApiRequestParams<ApiRequestMethod, TResponseBody>
-  ) => {
+  ): TResponseBody | null => {
     return this.responseBodyCache.get(
       apiRequestId(params)
     ) as TResponseBody | null
@@ -136,7 +137,7 @@ export class Api {
    */
   onCacheUpdate = <TResponseBody extends ResponseBody>(
     params: ApiRequestParams<ApiRequestMethod, TResponseBody>
-  ) => {
+  ): Observable<TResponseBody> => {
     return this.getCacheUpdateStream(params).observable
   }
 
